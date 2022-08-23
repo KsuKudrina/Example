09c7c5906f3f2b8d09d9e0fbff27d5017697bd69
Ksenia <ksenia.barteva@gmail.com>
2022-08-23 15:32:58 +0700

    Example005_CondotionIfElse

---
+++ Example005_CondotionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿//Используем if / else
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ксюня")
+{
+    Console.WriteLine("Ура! Это же Ксюня!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
