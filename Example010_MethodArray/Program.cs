dcb0995ddf4e023482fa8c3fedd079e9bbb8acd1
Ksenia <ksenia.barteva@gmail.com>
2022-08-24 19:47:15 +0700

    Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿//
+int[] array = {14, 62, 53, 49, 5, 16, 7, 28};
+
+int n = array.Length;
+int find = 28;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
